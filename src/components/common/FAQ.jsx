826927490455b708df16213d1d5294ef3ddd51a5
Sourabh <sourabhsahu69733@gmail.com>
2024-08-16 17:57:58 +0530

    fix: FAQ component - added key for listing

--- src/components/common/FAQ.jsx
+++ src/components/common/FAQ.jsx
@@ -13,47 +13,47 @@ import MuiAccordionDetails from '@mui/material/AccordionDetails';
 
 const Accordion = styled((props) => (
 	<MuiAccordion disableGutters elevation={0} square {...props} />
-  ))(({ theme }) => ({
+))(({ theme }) => ({
 	border: `1px solid ${theme.palette.divider}`,
 	'&:not(:last-child)': {
-	  borderBottom: 0,
+		borderBottom: 0,
 	},
 	'&::before': {
-	  display: 'none',
+		display: 'none',
 	},
-  }));
-  
-  const AccordionSummary = styled((props) => (
+}));
+
+const AccordionSummary = styled((props) => (
 	<MuiAccordionSummary
-	  expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
-	  {...props}
+		expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+		{...props}
 	/>
-  ))(({ theme }) => ({
+))(({ theme }) => ({
 	backgroundColor:
-	  theme.palette.mode === 'dark'
-		? 'rgba(255, 255, 255, .05)'
-		: 'rgba(0, 0, 0, .03)',
+		theme.palette.mode === 'dark'
+			? 'rgba(255, 255, 255, .05)'
+			: 'rgba(0, 0, 0, .03)',
 	flexDirection: 'row-reverse',
 	'& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-	  transform: 'rotate(90deg)',
+		transform: 'rotate(90deg)',
 	},
 	'& .MuiAccordionSummary-content': {
-	  marginLeft: theme.spacing(1),
+		marginLeft: theme.spacing(1),
 	},
-  }));
-  
-  const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
 	padding: theme.spacing(2),
 	borderTop: '1px solid rgba(0, 0, 0, .125)',
-  }));
+}));
 
 export default function FAQSection() {
 	const [expanded, setExpanded] = React.useState('')
 
 	const handleChange =
-	  (panel) => (event, newExpanded) => {
-		setExpanded(newExpanded ? panel : false);
-	  };
+		(panel) => (event, newExpanded) => {
+			setExpanded(newExpanded ? panel : false);
+		};
 	return (
 		<Container maxWidth="md">
 			<Typography variant="h4" gutterBottom align="center" color={"#ff6600"}>
@@ -63,20 +63,23 @@ export default function FAQSection() {
 				const panel = 'panel' + String(index + 1)
 				const clicked = panel == expanded
 				return (
-					<Accordion sx={{ marginBottom: "15px", boxShadow: "#f5f3f3 0px 5px 5px 0px" }} expanded={expanded === panel} onChange={handleChange(panel)}>
-						<AccordionSummary
-							expandIcon={<ExpandMoreIcon />}
-							// onClick={() => setClicked(!clicked)}
-							sx={{ boxShadow: "10px", color: clicked ? "#fff" : "#000", backgroundColor: clicked ? "#f1794a" : "#fff" }}
-						>
-							<Typography variant="subtitle1" style={{ fontWeight: "bold" }}>
-								{question}
-							</Typography>
-						</AccordionSummary>
-						<AccordionDetails>
-							<Typography>{answer}</Typography>
-						</AccordionDetails>
-					</Accordion>
+					<div key={"faq_section_" + index}>
+						<Accordion sx={{ marginBottom: "15px", boxShadow: "#f5f3f3 0px 5px 5px 0px" }} expanded={expanded === panel} onChange={handleChange(panel)}>
+							<AccordionSummary
+								expandIcon={<ExpandMoreIcon />}
+								// onClick={() => setClicked(!clicked)}
+								sx={{ boxShadow: "10px", color: clicked ? "#fff" : "#000", backgroundColor: clicked ? "#f1794a" : "#fff" }}
+							>
+								<Typography variant="subtitle1" style={{ fontWeight: "bold" }}>
+									{question}
+								</Typography>
+							</AccordionSummary>
+							<AccordionDetails>
+								<Typography>{answer}</Typography>
+							</AccordionDetails>
+						</Accordion>
+					</div>
+
 				);
 			})}
 		</Container>
