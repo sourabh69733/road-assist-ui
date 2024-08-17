967e9a17f5db76893a3f1188d4b97d8f1a4b3fa7
Sourabh <sourabhsahu69733@gmail.com>
2024-08-17 22:25:51 +0530

    feat: services in route

--- src/app.jsx
+++ src/app.jsx
@@ -5,24 +5,36 @@ import About from "./components/About";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import Profile from "./components/Profile";
-import { BatteryJumpstartService } from "./components/Services";
+import {
+  BatteryJumpstartService,
+  FlatTyreService,
+  FuelFillingService,
+  KeyLockoutService,
+  MechanicalFaultService,
+  VehicleTowingService,
+} from "./components/Services";
 import Footer from './components/Footer'
 
 const App = () => {
   return (
-      <Router>
-        <div style={{  }}>
+    <Router>
+      <div style={{}}>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/battery-jump-start" element={<BatteryJumpstartService />} />
+          <Route path="/tyre-puncture" element={<FlatTyreService />} />
+          <Route path="/fuel-delivery" element={<FuelFillingService />} />
+          <Route path="/key-lockout" element={<KeyLockoutService />} />
+          <Route path="/instant-car-repair" element={<MechanicalFaultService />} />
+          <Route path="/towing" element={<VehicleTowingService />} />
           <Route path="/about" element={<About />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
       <Footer />
-      </Router>
+    </Router>
   );
 };
 
